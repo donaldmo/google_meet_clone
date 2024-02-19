79b7e4e209176450d6ad04dc3d60e1aa62b02797
Donald Motswiri <domotswiri@gmail.com>
2024-02-20 00:42:41 +0200

    express server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+var app = express();
+var port = process.env.PORT || 3000
+
+var server = app.listen(port, function() {
+    console.log(`Listening on port ${port}`)
+});
+
+const io = require("socket.io")(server);
+app.use(express.static(path.join(__dirname, "")));
+
+io.on('connection', (socket) => {
+    console.log('socket id: ', socket.id)
+})
